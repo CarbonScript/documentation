59fe0c5fc1b77efcf9ff3f67bc4d761d408bbe82
Edward Zhang <643431636@qq.com>
2022-07-24 21:15:11 +0800

    fix website logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
         title: 'CarbonScript',
         logo: {
           alt: 'CarbonScript Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
